933c1154b144be6e2f18c0fe2bc506fd7a14c7a5
Vlad <wladimir.pushkutse@gmx.de>
2023-05-16 17:30:30 +0200

    Ready to Go

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.pfmartialhero {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+//    opens com.example.pfmartialhero to javafx.fxml;
+//    exports com.example.pfmartialhero;
+    opens View to javafx.fxml;
+    exports View;
+    opens Controller to javafx.fxml;
+    exports Controller;
+}
