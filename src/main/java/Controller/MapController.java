fbb36ba95be9a9ac663c8a4968be2ff8df58c489
Vlad <wladimir.pushkutse@gmx.de>
2023-06-03 19:23:36 +0200

    Threads to Go

--- src/main/java/Controller/MapController.java
+++ src/main/java/Controller/MapController.java
@@ -9,24 +9,20 @@ import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.VBox;
+import Controller.PlayerController;
 
 
 import java.io.IOException;
 
-public class MapController {
-
-    private double jump_amount= 50;
-    double GRAVITY = 2;
-    private double velocity=0;
+public class MapController  {
 
     @FXML
     protected void pause(){
         Main.startStage.close();
     }
 
-
     @FXML
-    public VBox background;
+    private VBox background;
 
     public void chooseMap(int mapNr){
 
@@ -38,8 +34,9 @@ public class MapController {
     }
 
 
+
     @FXML
-    protected void openMap1() throws IOException {
+    protected void openMap1()  throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/fxml/Map.fxml"));
         System.out.println(fxmlLoader.getLocation());
 
@@ -55,16 +52,19 @@ public class MapController {
         System.out.println(N1.getClass());
         System.out.println(N2.getClass());
 
-        Player player1= new Player("P1", N1);
-        Player player2= new Player("P2", N2);
+        PlayerController PC1= new PlayerController(N1, scene, 1);
+        PlayerController PC2= new PlayerController(N2, scene, 2);
 
-//       System.out.println(N1.getClass().toString());
-
-        addMovement(scene,N1, N2);
-        setJump(N1);
+        PC1.start();
+        PC2.start();
 
         Main.startStage.setScene(scene);
         Main.startStage.show();
+
+//        Wohin mit den Playern?!
+
+//        Player player1= new Player("P1", N1);
+//        Player player2= new Player("P2", N2);
 
         MapController MC1=fxmlLoader.getController();
         MC1.chooseMap(1);
@@ -78,61 +78,6 @@ public class MapController {
     @FXML
     protected void initialize() {
 
-    }
-
-    public void moveRight(Node n){
-        Double x= n.getTranslateX();
-        n.setTranslateX(x+50);
-    }
-
-    public void moveLeft(Node n){
-        Double x= n.getTranslateX();
-        n.setTranslateX(x+50);
-    }
-
-    public void addMovement(Scene s, Node n1, Node n2){
-
-        s.setOnKeyPressed(event -> {
-            if (event.getCode() == KeyCode.D) {
-                // Bewege den Kreis um MOVE_AMOUNT Pixel nach rechts
-                n1.setTranslateX(n1.getTranslateX() + 10);
-            }
-            if (event.getCode() == KeyCode.A) {
-                // Bewege den Kreis um MOVE_AMOUNT Pixel nach rechts
-                n1.setTranslateX(n1.getTranslateX() - 10);
-            }
-            if (event.getCode() == KeyCode.RIGHT) {
-                // Bewege den Kreis um MOVE_AMOUNT Pixel nach rechts
-                n2.setTranslateX(n2.getTranslateX() + 10);
-            }
-            if (event.getCode() == KeyCode.LEFT) {
-                // Bewege den Kreis um MOVE_AMOUNT Pixel nach rechts
-                n2.setTranslateX(n2.getTranslateX() - 10);
-            }
-            if (event.getCode() == KeyCode.W) {
-                // Führe einen Sprung aus, wenn die Taste "w" gwedrückt wird
-                velocity= -jump_amount;
-            }
-        });
-    }
-
-    public void setJump(Node n){
-        // Animationsschleife, um den Sprung und die Schwerkraft anzuwenden
-        javafx.animation.AnimationTimer timer = new javafx.animation.AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                // Anwenden der Schwerkraft
-                velocity += GRAVITY;
-                n.setTranslateY(n.getTranslateY() + velocity);
-                // Überprüfen, ob das Rechteck den Boden berührt
-                if (n.getTranslateY() >=0) {
-                    // Zurücksetzen der Vertikalgeschwindigkeit
-                    velocity = 0;
-                    n.setTranslateY(0);
-                }
-            }
-        };
-        timer.start();
     }
 
 }
---
+++ src/main/java/Controller/PlayerController.java
@@ -0,0 +1,93 @@
+package Controller;
+
+import Application.Main;
+import Model.Player;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.VBox;
+
+public class PlayerController extends Thread {
+    private double jump_amount= 50;
+    double GRAVITY = 2;
+    private double velocity=0;
+    private Node N;
+
+    private int player;
+
+    private Scene S;
+
+
+    public PlayerController(Node N, Scene S, int player){
+        this.N=N;
+        this.S=S;
+        this.player=player;
+    }
+
+
+    public void run() {
+        if (this.player==1){
+            addTransP1();
+        }else addTransP2();
+        setJump(this.N);
+        System.out.println(this.getName()+ "gestartet");
+    }
+
+
+    public void setJump(Node n){
+        // Animationsschleife, um den Sprung und die Schwerkraft anzuwenden
+        javafx.animation.AnimationTimer timer = new javafx.animation.AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                // Anwenden der Schwerkraft
+                velocity += GRAVITY;
+                n.setTranslateY(n.getTranslateY() + velocity);
+                // Überprüfen, ob das Rechteck den Boden berührt
+                if (n.getTranslateY() >=0) {
+                    // Zurücksetzen der Vertikalgeschwindigkeit
+                    velocity = 0;
+                    n.setTranslateY(0);
+                }
+            }
+        };
+        timer.start();
+    }
+
+    private void addTransP1(){
+        this.S.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.D) {
+                // Bewege den Kreis um MOVE_AMOUNT Pixel nach rechts
+                this.N.setTranslateX(this.N.getTranslateX() + 10);
+            }
+            if (event.getCode() == KeyCode.A) {
+                // Bewege den Kreis um MOVE_AMOUNT Pixel nach rechts
+                this.N.setTranslateX(this.N.getTranslateX() - 10);
+            }
+            if (event.getCode() == KeyCode.W) {
+                // Führe einen Sprung aus, wenn die Taste "w" gwedrückt wird
+                velocity= -jump_amount;
+            }
+        });
+    }
+
+    private void addTransP2(){
+        this.S.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.RIGHT) {
+                // Bewege den Kreis um MOVE_AMOUNT Pixel nach rechts
+                this.N.setTranslateX(this.N.getTranslateX() + 10);
+            }
+            if (event.getCode() == KeyCode.LEFT) {
+                // Bewege den Kreis um MOVE_AMOUNT Pixel nach rechts
+                this.N.setTranslateX(this.N.getTranslateX() - 10);
+            }
+            if (event.getCode() == KeyCode.UP) {
+                // Führe einen Sprung aus, wenn die Taste "w" gwedrückt wird
+                velocity= -jump_amount;
+            }
+        });
+    }
+
+}
