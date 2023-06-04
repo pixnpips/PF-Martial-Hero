533d4e8ea369991b525003b5d5671ec670b0df66
Ribuah <91778175+Ribuah@users.noreply.github.com>
2023-06-05 01:19:39 +0200

    !

--- src/main/java/Application/Main.java
+++ src/main/java/Application/Main.java
@@ -3,6 +3,7 @@ package Application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -13,6 +14,7 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/fxml/PlayerMovement.fxml"));
         System.out.println(fxmlLoader.getLocation());
         Scene scene = new Scene(fxmlLoader.load(), 1000, 500);
+        scene.setFill(Color.rgb(50,50,50));
         scene.getRoot().requestFocus();
         stage.setTitle("Martial Hero");
         stage.setScene(scene);
--- src/main/java/Controller/PlayerController.java
+++ src/main/java/Controller/PlayerController.java
@@ -2,25 +2,39 @@ package Controller;
 
 import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class PlayerController {
 
+    private static final int FRAME_WIDTH = 200;
+    private static final int FRAME_HEIGHT = 200;
+    private static final int FRAME_DURATION = 75000000; // Duration in nanoseconds
+    private static final int NUM_FRAMES_IDLE = 8;
+    private static final int NUM_FRAMES_RUN = 8;
+    private static final int NUM_FRAMES_ATTACK = 6;
+    private static final int NUM_FRAMES_TAKE_HIT = 4;
+    private static final int NUM_FRAMES_DEATH = 6;
+    private static final int NUM_FRAMES_JUMP = 2;
+
     @FXML
     private Canvas canvas;
 
-    private static final int FRAME_WIDTH = 200;
-    private static final int FRAME_HEIGHT = 200;
-    private static int NUM_FRAMES = 0;
-    private static final int FRAME_DURATION = 100000000; // Duration in nanoseconds
+    private List<Image> idleFrames;
+    private List<Image> runFrames;
+    private List<Image> attack1Frames;
+    private List<Image> attack2Frames;
+    private List<Image> takeHitFrames;
+    private List<Image> deathFrames;
+    private List<Image> jumpFrames;
 
     private List<Image> frames;
     private int currentFrameIndex;
@@ -30,15 +44,22 @@ public class PlayerController {
     private AnimationTimer timer;
     private boolean isMoving = false;
 
+    private double playerVelocityX = 0;
+    private double playerVelocityY = 0;
+    private double playerPositionX;
+    private double playerPositionY;
+
     public void initialize() {
-        NUM_FRAMES = 8;
         // Load the sprite frames
-        frames =  new ArrayList<>();
-        for (int i = 1; i <= NUM_FRAMES; i++) {
-            String imagePath = "/images/sprites/char1/Idle_" + i + ".png";
-            Image frame = new Image(Objects.requireNonNull(getClass().getResourceAsStream(imagePath)));
-            frames.add(frame);
-        }
+        idleFrames = loadFrames("/images/sprites/char1/Idle_", NUM_FRAMES_IDLE);
+        runFrames = loadFrames("/images/sprites/char1/Run_", NUM_FRAMES_RUN);
+        attack1Frames = loadFrames("/images/sprites/char1/Attack1_", NUM_FRAMES_ATTACK);
+        attack2Frames = loadFrames("/images/sprites/char1/Attack2_", NUM_FRAMES_ATTACK);
+        takeHitFrames = loadFrames("/images/sprites/char1/Take Hit_", NUM_FRAMES_TAKE_HIT);
+        deathFrames = loadFrames("/images/sprites/char1/Death_", NUM_FRAMES_DEATH);
+        jumpFrames = loadFrames("/images/sprites/char1/Jump_", NUM_FRAMES_JUMP);
+
+        frames = idleFrames;
 
         {
             // Create the animation timer
@@ -60,7 +81,7 @@ public class PlayerController {
                         gc.drawImage(frames.get(currentFrameIndex), 0, 0);
 
                         // Update the frame index
-                        currentFrameIndex = (currentFrameIndex + 1) % NUM_FRAMES;
+                        currentFrameIndex = (currentFrameIndex + 1) % frames.size();
 
                         // Remember the current time for the next frame
                         lastFrameTime = currentTime;
@@ -86,28 +107,24 @@ public class PlayerController {
         timer.start();
     }
 
-    private double playerVelocityX = 0;
-    private double playerVelocityY = 0;
-    private double playerPositionX;
-    private double playerPositionY;
-
     // Hier werden die Tasteneingaben des Spielers erfasst
     @FXML
     void handleKeyPressed(KeyEvent event) {
 
         // Hier werden die Geschwindigkeit und die Position des Spielers initialisiert
-        double playerSpeed;
-        playerSpeed = 1;
-        if (event.getCode() == KeyCode.A) {
+        double playerSpeed = 1;
 
-            playerVelocityX = -playerSpeed;
-        } else if (event.getCode() == KeyCode.D) {
+        if (event.getCode() == KeyCode.A || event.getCode() == KeyCode.D) {
             isMoving = true;
-            playerVelocityX = playerSpeed;
+            frames = runFrames;
+            playerVelocityX = (event.getCode() == KeyCode.A) ? -playerSpeed : playerSpeed;
+            canvas.setScaleX((event.getCode() == KeyCode.A) ? -1 : 1);
         } else if (event.getCode() == KeyCode.W) {
             playerVelocityY = -playerSpeed;
         } else if (event.getCode() == KeyCode.S) {
             playerVelocityY = playerSpeed;
+        } else if (event.getCode() == KeyCode.SPACE) {
+            //frames = jumpFrames;
         }
     }
 
@@ -115,14 +132,14 @@ public class PlayerController {
     @FXML
     void handleKeyReleased(KeyEvent event) {
 
-        if (event.getCode() == KeyCode.A) {
+        if (event.getCode() == KeyCode.A || event.getCode() == KeyCode.D) {
+            isMoving = false;
+            frames = idleFrames;
             playerVelocityX = 0;
-        } else if (event.getCode() == KeyCode.D) {
+        } else if (event.getCode() == KeyCode.W || event.getCode() == KeyCode.S) {
             playerVelocityX = 0;
-        } else if (event.getCode() == KeyCode.W) {
-            playerVelocityY = 0;
-        } else if (event.getCode() == KeyCode.S) {
-            playerVelocityY = 0;
+        } else if (event.getCode() == KeyCode.SPACE) {
+            playerVelocityX = 0;
         }
     }
 
@@ -130,13 +147,25 @@ public class PlayerController {
     @FXML
     void handleMouseClicked(MouseEvent event) {
         // Hier wird die Angriffsanimation des Spielers ausgelöst
-        // ...
 
     }
 
+    private List<Image> loadFrames(String imagePathPrefix, int numFrames) {
+        // Load the sprite frames
+        List<Image> frames = new ArrayList<>();
+        for (int i = 1; i <= numFrames; i++) {
+            String imagePath = imagePathPrefix + i + ".png";
+            Image frame = new Image(Objects.requireNonNull(getClass().getResourceAsStream(imagePath)));
+            frames.add(frame);
+        }
+        return frames;
+    }
+
     // Hier wird der Timer gestoppt
-    public void stop() {
+    public void stopAnimation() {
         timer.stop();
+        frames = idleFrames;
+        currentFrameIndex = 0;
     }
 
 }
