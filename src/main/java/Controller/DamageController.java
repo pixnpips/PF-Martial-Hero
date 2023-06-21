b902f21dacd8e8d369d0c9361f7eba00ff0351ac
Vlad <wladimir.pushkutse@gmx.de>
2023-06-21 11:09:26 +0200

    DamageControl 2.0 Treffer werden erkannt

--- src/main/java/Controller/DamageController.java
+++ src/main/java/Controller/DamageController.java
@@ -36,23 +36,43 @@ public class DamageController implements PropertyChangeListener {
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        System.out.println(evt.getPropertyName());
+//        System.out.println(evt.getPropertyName());
         switch (evt.getPropertyName()){
-            case "x_N1": System.out.println("Neuer X Wert von Node 1:"+ evt.getNewValue());
+            case "x_N1":
+//                System.out.println("Neuer X Wert von Node 1:"+ evt.getNewValue());
+            this.x_P1=(double) evt.getNewValue();
             break;
-            case "x_N2": System.out.println("Neuer X Wert von Node 2:"+ evt.getNewValue());
+            case "x_N2":
+//ad
+            this.x_P2=(double) evt.getNewValue();
             break;
-            case "y_N1": System.out.println("Neuer Y Wert von Node 1:"+ evt.getNewValue());
+            case "y_N1":
+//                System.out.println("Neuer Y Wert von Node 1:"+ evt.getNewValue());
+            this.y_P1=(double) evt.getNewValue();
             break;
-            case "y_N2": System.out.println("Neuer Y Wert von Node 2:"+ evt.getNewValue());
+            case "y_N2":
+//
+            this.y_P2=(double) evt.getNewValue();
             break;
-            case "attack11": System.out.println("Player 1 Attacke 1 :"+ evt.getNewValue());
+            case "attack11":
+                System.out.println("Player 1 Attacke 1 :"+ evt.getNewValue());
+                this.getHitP2();
+            this.attack1_P1=(boolean) evt.getNewValue();
             break;
-            case "attack21": System.out.println("Player 1 Attacke 2 :"+ evt.getNewValue());
+            case "attack21":
+                System.out.println("Player 1 Attacke 2 :"+ evt.getNewValue());
+                this.getHitP2();
+            this.attack2_P1=(boolean) evt.getNewValue();
             break;
-            case "attack12": System.out.println("Player 2 Attacke 1 :"+ evt.getNewValue());
+            case "attack12":
+                System.out.println("Player 2 Attacke 1 :"+ evt.getNewValue());
+            this.attack1_P2=(boolean) evt.getNewValue();
+                this.getHitP1();
                 break;
-            case "attack22": System.out.println("Player 2 Attacke 2 :"+ evt.getNewValue());
+            case "attack22":
+                System.out.println("Player 2 Attacke 2 :"+ evt.getNewValue());
+            this.attack2_P2=(boolean) evt.getNewValue();
+                this.getHitP1();
                 break;
         }
     }
@@ -63,6 +83,20 @@ public class DamageController implements PropertyChangeListener {
 
     public void reducePb2(){
         this.pb1.setProgress(this.pb1.getProgress()-10);
+    }
+
+    public void getHitP1(){
+        System.out.println(Math.abs(x_P2-x_P1));
+        if ((Math.abs(x_P2-x_P1)<630&&Math.abs(y_P1-y_P2)<300)&&(attack1_P2||attack2_P2)){
+            System.out.println("Player 1 wurde getroffen");
+        }
+    }
+
+    public void getHitP2(){
+        System.out.println(Math.abs(x_P2-x_P1));
+        if ((Math.abs(x_P2-x_P1)<630&&Math.abs(y_P1-y_P2)<300)&&(attack1_P1||attack2_P1)){
+            System.out.println("Player 2 wurde getroffen");
+        }
     }
 
     public void die(Player p){
