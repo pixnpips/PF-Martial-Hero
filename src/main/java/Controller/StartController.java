e501fa3b363c0be4ef79758975294c81a95560ec
s78677 <s78677@bht-berlin.de>
2023-06-22 18:20:04 +0200

    Merge V2

--- src/main/java/Controller/StartController.java
+++ src/main/java/Controller/StartController.java
@@ -2,6 +2,8 @@ package Controller;
 
 import View.FxmlView;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import View.Main;
 
@@ -12,14 +14,17 @@ public class StartController {
     @FXML
     private Label welcomeText;
     private FxmlView View;
+
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
     @FXML
     public void exit(){
         Main.exit();
     }
+
     @FXML
     protected void openMapChoice() throws IOException {
         View = new FxmlView();
