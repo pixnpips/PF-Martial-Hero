4549f7aa027f2e226bba29c237e8bcf53f61425b
s78677 <s78677@bht-berlin.de>
2023-06-24 14:07:41 +0200

    NameMenu V1, PlayerController V1

---
+++ src/main/java/Controller/PlayerController.java
@@ -0,0 +1,52 @@
+package Controller;
+
+import Model.Player;
+import View.FxmlView;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class PlayerController {
+    @FXML
+    TextField input1;
+    @FXML
+    TextField input2;
+
+    private String name1;
+    private String name2;
+
+    private static Player player1;
+    private static Player player2;
+
+    private FxmlView View;
+
+    public PlayerController(){}
+
+    public void getNames(){
+        name1 = input1.getText();
+        name2 = input2.getText();
+        if (name1.isEmpty() || name2.isEmpty()){
+            System.out.println("no input");
+        }
+        else{
+            createPlayers(name1, name2);
+            System.out.println(name1);
+            System.out.println(name2);
+            try {
+                openMapChoice();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    public void createPlayers(String name1, String name2){
+        player1 = new Player(name1);
+        player2 = new Player(name2);
+    }
+    public void openMapChoice() throws IOException {
+        View = new FxmlView();
+        View.load("/fxml/MapChoice.fxml", "Choose your Map");
+    }
+
+}
--- src/main/java/Controller/StartController.java
+++ src/main/java/Controller/StartController.java
@@ -2,8 +2,6 @@ package Controller;
 
 import View.FxmlView;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import View.Main;
 
@@ -26,8 +24,8 @@ public class StartController {
     }
 
     @FXML
-    protected void openMapChoice() throws IOException {
+    protected void openNameMenu() throws IOException {
         View = new FxmlView();
-        View.load("/fxml/MapChoice.fxml", "Choose your Map");
+        View.load("/fxml/NameMenu.fxml", "Choose Player Names");
     }
 }
